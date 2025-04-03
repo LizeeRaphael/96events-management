a9cf3fcf5761ccb8fd1e762ea4599e2a45a6e20b
Lizee <111397883+LizeeRaphael@users.noreply.github.com>
2025-04-03 15:19:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+  
+ 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js">
+</script>
+<script>
+  $(".testimonial_slider_area").owlCarousel({
+autoplay:true,
+slideSpeed:3000,
+item:3,
+nav:true,
+navText:[' <i class="fa fa-arrow-left"></i>',' <i class="fa fa-arrow-right"></i>'],
+margin:30,
+dots:false,
+responsive:{
+  320:{
+    items:1
+  },
+  767:{
+    items:2
+  },
+  600:{
+     items:2 
+  },
+1000:{
+  items:3
+}
+}
+});
+
+</script>
+<section id="testimonial_area">
+  <div class="container-container">
+    
+    <div class="rows">
+      <div class="col-md-12">
+<div class="testimonial_slider_area text-center owl-carousel">
+<div class="box-area">
+  <div class="img-area">
+    <img src="images/lizee.png.JPG" width="300px" height="300px">
+  </div>
+  <h5>Person's Name</h5>
+  <span>Designation Goes Here</span>
+  <p class="content"> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo natus fugit consequatur, aliquid labore debitis, sapiente provident distinctio nisi illo libero, 
+    deserunt sequi ullam ex nobis officiis harum minima!
+  </p>
+  <h6 class="socials">
+    <i class="fa fa-instagram"></i>
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-linkedin "></i>
+    <i class="fa fa-youtube"></i>
+  </h6>
+</div>
+<div class="box-area">
+  <div class="img-area">
+    <img src="images/lizee.png.JPG" width="300px" height="300px">
+  </div>
+  <h5>Person's Name</h5>
+  <span>Designation Goes Here</span>
+  <p class="content"> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo natus fugit consequatur, aliquid labore debitis, sapiente provident distinctio nisi illo libero, 
+    deserunt sequi ullam ex nobis officiis harum minima!
+  </p>
+  <h6 class="socials">
+    <i class="fa fa-instagram"></i>
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-linkedin "></i>
+    <i class="fa fa-youtube"></i>
+  </h6>
+</div>
+<div class="box-area">
+  <div class="img-area">
+    <img src="images/lizee.png.JPG" width="300px" height="300px">
+  </div>
+  <h5>Person's Name</h5>
+  <span>Designation Goes Here</span>
+  <p class="content"> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo natus fugit consequatur, aliquid labore debitis, sapiente provident distinctio nisi illo libero, 
+    deserunt sequi ullam ex nobis officiis harum minima!
+  </p>
+  <h6 class="socials">
+    <i class="fa fa-instagram"></i>
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-linkedin "></i>
+    <i class="fa fa-youtube"></i>
+  </h6>
+</div>
+<div class="box-area">
+  <div class="img-area">
+    <img src="images/lizee.png.JPG" width="300px" height="300px">
+  </div>
+  <h5>Person's Name</h5>
+  <span>Designation Goes Here</span>
+  <p class="content"> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo natus fugit consequatur, aliquid labore debitis, sapiente provident distinctio nisi illo libero, 
+    deserunt sequi ullam ex nobis officiis harum minima!
+  </p>
+  <h6 class="socials">
+    <i class="fa fa-instagram"></i>
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-linkedin "></i>
+    <i class="fa fa-youtube"></i>
+  </h6>
+</div>
+</div>
+      </div>
+    </div>
+  </div>
+</section> 
+
+
+
+<link rel="stylesheet" href="https//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/boostrap.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax//libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax//libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
+ 
+  
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
+    
+  
+     
